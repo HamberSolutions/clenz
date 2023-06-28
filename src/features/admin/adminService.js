cea7945b48877c2f4931dacab76c3677476b22d1
usmanmani010 <usmanmani010@gmail.com>
2023-06-28 20:53:23 +0500

    clenz

---
+++ src/features/admin/adminService.js
@@ -0,0 +1,41 @@
+import axios from "axios";
+import { USERS_BASE_URL } from "../../components/Constants/Config/config.dev";
+
+
+const API_URL_1 = `${USERS_BASE_URL}/order/book-order`;
+
+const API_URL_2 = `${USERS_BASE_URL}/order/getslots/2023/6`;
+
+const API_URL_3 = `${USERS_BASE_URL}/order/getslots/2023/6`;
+
+const orderbook = async (orderData) => {
+  try {
+    const response = await axios.post(API_URL_1, orderData);
+    return response.data;
+  } catch (error) {
+    console.log("error",error)
+    return error;
+  }
+};
+
+const getslots = async (slotsData) => {
+  try {
+    const response = await axios.get(API_URL_2, slotsData);
+
+    if (response.data) {
+      localStorage.setItem("user", JSON.stringify(response.data));
+    }
+console.log("response",response.data)
+    return response.data;
+  } catch (error) {
+    console.log("error",error)
+    return error;
+  }
+};
+
+const dashboardService = {
+	orderbook,
+	getslots,
+};
+
+export default dashboardService;

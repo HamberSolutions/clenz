5d44b42c5598a1d6158f8c71912286b4401efeaf
usmanmani010 <usmanmani010@gmail.com>
2023-06-28 21:18:48 +0500

    clenz12

--- src/components/Adminpanel/Admincomp/Neworder.jsx
+++ src/components/Adminpanel/Admincomp/Neworder.jsx
@@ -1,16 +1,98 @@
 import React from 'react'
-import Table from "../../Common/Table/Table";
+import { Table } from "react-bootstrap"
 
 const Neworder = () => {
-  return (
+  const TableHeading = [
+    {
+        Id: "1",
+        name: "Serial #"
+    },
+    {
+        Id: "2",
+        name: "Customer Name"
+    },
+    {
+        Id: "3",
+        name: "Address"
+    },
+    {
+        Id: "4",
+        name: "Contact #"
+    },
+    {
+        Id: "5",
+        name: "Package Name"
+    },
+    {
+        Id: "6",
+        name: "Price"
+    },
+    {
+      Id: "7",
+      name: "Status"
+  },
+]
+const TableData = [
+    {
+        Id: "1",
+        name: "1"
+    },
+    {
+        Id: "2",
+        name: "Hafiz Abu Baker"
+    },
+    {
+        Id: "3",
+        name: "Johar Town Lahore"
+    },
+    {
+        Id: "4",
+        name: "03321718144"
+    },
+    {
+        Id: "5",
+        name: "Relaxation Massage - 60 minutes"
+    },
+    {
+        Id: "6",
+        name: "AED 249"
+    },
+    {
+      Id: "7",
+      name: "Pending"
+  },
+]
+return (
     <>
-      <div>
-        <div>Neworder</div>
-        <Table />
-      </div>
-    </>
+        <div className="table_section">
+            <div className="table_div">
+                <Table responsive >
 
-  )
-}
+                    <thead className='tb_header_bg'>
+                        <tr>
+
+                            {TableHeading.map((data, index) => (
+                                <th className='obs_table_headder' key={index}>{data.name}</th>
+                            ))}
+                        </tr>
+                    </thead>
+
+                    <tbody>
+
+
+                        <tr>
+
+                            {TableData.map((data, index) => (
+                                <td className='obs_table_content' key={index}>{data.name}</td>
+                            ))}
+                        </tr>
+                    </tbody>
+                </Table>
+            </div>
+        </div>
+    </>
+);
+};
 
 export default Neworder
+
